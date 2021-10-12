ba465deec4b777ba155d42c32ae7f95c17e12283
Yasho <90166733+Yasho022@users.noreply.github.com>
2021-10-12 17:08:19 +0530

    Update push-pr.bat

--- push-pr.bat
+++ push-pr.bat
@@ -5,13 +5,13 @@ set flag=%2
 cd /d %~dp0
 
 If "%1"=="" (
-    echo Please Provide the Name of the Repository
+    python push-pr.py
 ) else ( 
     if "%2"=="" (
         python push-pr.py %fn% %flag%
         ) else (
             if "%2"=="l" (
-               python C:\Users\wired\pycmd\Scripts\push-pr.py %fn%
+               python push-pr.py %fn%
             )
         )
     )
